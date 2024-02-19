079f8e470db05e1236730b3ec8a6d965c14e5151
Valdo <vteixeiraromao@gmail.com>
2024-02-19 17:21:43 +0000

    Add Campus map and preview between maps

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,18 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    WA.room.area.onEnter('exitToWorkshops').subscribe(() => {
+        WA.room.showLayer('exitArrow');
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onEnter('exitToCampus').subscribe(() => {
+        WA.room.showLayer('exitArrow');
+    })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,12 +23,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
